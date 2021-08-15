91eaddd6bfe0adc4067255289e71d4a19a330ab7
PengChuqing <2716899503@qq.com>
2021-08-16 00:16:15 +0800

    整合nacos失败

--- src/api/edu/video.js
+++ src/api/edu/video.js
@@ -16,5 +16,12 @@ export default {
       method: 'delete'
     })
   },
+  //删除视频
+  deleteAliyunvod(id) {
+    return request({
+        url: '/eduvod/video/removeAlyVideo/'+id,
+        method: 'delete'
+      })
+}
   
 }
--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -170,6 +170,29 @@ export default {
   },
 
   methods: {
+    //点击确定调用的方法
+        handleVodRemove() {
+            //调用接口的删除视频的方法
+            video.deleteAliyunvod(this.video.videoSourceId)
+                .then(response => {
+                    //提示信息
+                    this.$message({
+                        type: 'success',
+                        message: '删除视频成功!'
+                    });
+                    //把文件列表清空
+                    this.fileList = []
+                    //把video视频id和视频名称值清空
+                    //上传视频id赋值
+                    this.video.videoSourceId = ''
+                    //上传视频名称赋值
+                    this.video.videoOriginalName = ''
+                })
+        },
+        //点击×调用这个方法
+        beforeVodRemove(file,fileList) {
+            return this.$confirm(`确定移除 ${ file.name }？`);
+        },
     //上传视频成功调用的方法
         handleVodUploadSuccess(response, file, fileList) {
             //上传视频id赋值
