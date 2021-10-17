fe67e3073b04fe00a294f7a5ad9d2231d6620143
PengChuqing <2716899503@qq.com>
2021-10-17 23:59:23 +0800

    数据统计

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -158,7 +158,8 @@ export default {
       fileList: [],//上传文件列表
       // BASE_API: process.env.BASE_API // 接口API地址
       // process.env.BASE_API用不了，暂时先这样试一下
-      BASE_API: 'http://localhost:9001'
+      BASE_API: process.env.VUE_APP_BASE_API
+      // BASE_API: 'http://localhost:9001'
     };
   },
 
